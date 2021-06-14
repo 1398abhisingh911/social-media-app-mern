e48befc2cf6fe3765816f56a488722ea1c86663d
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-06-14 22:33:58 +0530

    Login+rEGISTRATION DONE

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import Home from "../src/pages/home/Home";
 import Login from "../src/pages/login/Login";
 import Register from "../src/pages/register/Register";
-
+import { Redirect } from "react-router";
 import Profile from "../src/pages/profile/Profile";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { AuthContext } from "./context/AuthContext";
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home />
+          {user ? <Home /> : <Register />}
         </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
+        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
         <Route path="/register">
-          <Register />
+          {user ? <Redirect to="/" /> : <Register />}
         </Route>
         <Route path="/profile/:username">
           <Profile />
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useRef } from "react";
 import "./register.css";
+import { useHistory } from "react-router-dom";
+
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const history = useHistory();
+
+  const handleClick = async e => {
+    e.preventDefault();
+    if (passwordAgain.current.value !== password.current.value) {
+      passwordAgain.current.setCustomValidity("Passwords don't match");
+    } else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value
+      };
+      try {
+        await axios.post("/auth/register", user);
+        history.push("/login");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -11,15 +39,41 @@ export default function Register() {
           </span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
-            <input placeholder="Username" className="loginInput" />
-            <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Password Again" className="loginInput" />
-            <button className="loginButton">Sign Up</button>
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Username"
+              ref={username}
+              className="loginInput"
+              required
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              ref={email}
+              className="loginInput"
+            />
+            <input
+              placeholder="Password"
+              ref={password}
+              required
+              type="password"
+              className="loginInput"
+              minLength="6"
+            />
+            <input
+              type="password"
+              placeholder="Password Again"
+              ref={passwordAgain}
+              required
+              className="loginInput"
+            />
+            <button className="loginButton" type="submit">
+              Sign Up
+            </button>
 
             <button className="loginRegisterButton">Login into account</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
