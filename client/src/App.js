4bed2ddad80ba2f7bc63f1aefcc017eae915d513
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-06-14 16:44:27 +0530

    Register and Login PAGE CREATED with conditional rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
 import Home from "../src/pages/home/Home";
-import Profile from "../src/pages/profile/Profile";
-import Login from "../src/pages/login/Login";
-
-import Register from "../src/pages/register/Register";
 function App() {
   return (
     <div>
-      <Register />
+      <Home />
     </div>
   );
 }
