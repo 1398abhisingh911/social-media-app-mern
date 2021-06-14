924042108ec1c1aac949a968dd286bead34dc6f1
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-06-14 17:58:40 +0530

    PROXY TO BACKEND DONE

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./topbar.css";
 import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
-        <span className="logo">Green.io</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Green.io</span>
+        </Link>
       </div>
       <div className="topbarCenter">
         <div className="searchbar">
