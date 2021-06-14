1d8f697ec36cd6147b08ac169b122a6d75ccd25b
Abhijeet Kumar Singh <1398Aaabhisingh911@gmail.com>
2021-06-14 13:11:40 +0530

    FEED and RIGHTBAR Component Done

---
+++ client/src/components/post/Post.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./post.css";
+import { MoreVert } from "@material-ui/icons";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="/assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="postUsername">Rohit</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey!It's my first Post</span>
+          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="/assets/like.png" alt="" />
+            <img className="likeIcon" src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">32 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postComentRight">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,54 @@
 import React from "react";
 import "./rightbar.css";
+
 export default function Rightbar() {
-  return <div className="rightbar">rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Ritwik Mullah</b> and <b>3 other friends</b> have birthday today.
+          </span>
+        </div>
+        <img className="rightbarAd" src="/assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Akshat Singh</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Akshat Singh</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                src="assets/person/3.jpeg"
+                alt=""
+                className="rightbarProfileImg"
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Akshat Singh</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
